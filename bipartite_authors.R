c6b0df3092908dcc62e0530ab9cfc5b185cf2c05
rustyBilges <tophermcw@gmail.com>
2017-05-18 17:26:30 -0500

    Finished section A, needs proof reading. Beginning section B.

--- bipartite_authors.R
+++ bipartite_authors.R
@@ -6,8 +6,17 @@ getwd()
 df=read.csv(file='R_edges.csv')
 df$newcol <- rep(1,nrow(df)) # make new column
 web=frame2webs(df, varnames=c("PUBLICATION_ID", "PERSON_ID","newcol"))
-
 plotweb(web$`1`, col.interaction='purple')
+
+web=read.csv(file='r_edges_p9742.csv')
+plotweb(web[2], col.interaction='grey', low.spacing=0.1, bor.col.interaction='black', col.high='blue', col.low='green', bor.col.high='black', bor.col.low='black', low.lablength=0, high.lablength=0)
+
+png("single_auhtor.png", width=1800, height=1200)
+dx <- dev.cur()
+dev.set(2)
+dev.copy(which=dx)
+dev.set(dx)
+dev.off()
 
 #visweb(web$`1`)
 bw = betweenness_w(web$`1`)
--- pub_net_script3.py
+++ pub_net_script3.py
@@ -4,15 +4,24 @@ from graphlab import aggregate as agg
 import itertools
 
 authors=graphlab.SFrame('./170331_PURE_Data_Challenge/PURE Data Challenge/authors.csv')
+pub_authors = authors.groupby(key_columns='PUBLICATION_ID', operations={'authors':agg.CONCAT('PERSON_ID')})
 
-pub_authors = authors.groupby(key_columns='PERSON_ID', operations={'publications':agg.CONCAT('PUBLICATION_ID')})
 
-solo_count = 0
-#links = dict()
+f=open('./publication_net_links_dict.pkl', 'rb')
+_pub_links = pickle.load(f)
+f.close()
 
-links = [l for pub in pub_authors for l in itertools.combinations(pub['publications'],2)]
-print "Established %d links in the publication network." %len(links)
+#weights = [len([author for author in pub_authors.filter_by(column_name='PUBLICATION_ID',values=link)['authors'][0] if author in pub_authors.filter_by(column_name='PUBLICATION_ID',values=link)['authors'][1]]) for link in _pub_links]
 
-f = open('publication_net_links_dict.pkl', 'wb')
+weights = [len([author for author in pub_authors[pub_authors['PUBLICATION_ID']==link[0]] if author in pub_authors[pub_authors['PUBLICATION_ID']==link[1]]]) for link in _pub_links]
+
+
+f = open('publication_net_links_weights.pkl', 'wb')
+pickle.dump(file=f,obj=weights)
+f.close()
+
+links = dict(zip(_pub_links, weights))
+
+f = open('publication_net_links_dict_comp.pkl', 'wb')
 pickle.dump(file=f,obj=links)
 f.close()
